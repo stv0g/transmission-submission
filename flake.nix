e729ac248b16704103eee640362689b32d802382
Steffen Vogel <post@steffenvogel.de>
2024-04-15 06:04:07 +0200

    nix: Add Flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2023 Steffen Vogel <post@steffenvogel.de>
+# SPDX-License-Identifier: Apache-2.0
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              pkg-config
+              clang
+              go_1_22
+              golangci-lint
+              reuse
+            ];
+        };
+
+        formatter = nixpkgs.nixfmt-rfc-style;
+      }
+    );
+}
